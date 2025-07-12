1d64101038cc90547b2620e516bcf061ca02feb1
AႦԋιʂԋҽƙ ƙυɱαɾ ραƚҽʅ <159283155+abhishek4901@users.noreply.github.com>
2025-07-13 01:52:16 +0530

    .

--- Array/InsertElementatGivenPosition3DM4.c
+++ Array/InsertElementatGivenPosition3DM4.c
@@ -10,12 +10,16 @@ int main()
         scanf("%d", &arr[i]);
     printf("Enter the position of element: ");
     scanf("%d", &pos);
+    if (pos <= size ){
     printf("Enter the element to insert: ");
     scanf("%d", &x);
-    for (i = pos - 1; i < size ; i--) // travese the array positi
-    arr[pos - 1] = x;                     // insert the element at given position
+    for (i = pos - 1; i < size ; i++) // travese the array positi
+   arr[i+1]= arr[i];
+   arr[pos - 1] = x; 
+    size++;// insert the element at given position
     printf("Array after insertion: ");
     for (i = 0; i < size; i++)
-        printf("%d ", arr[i]);
+        printf("%d ", arr[i]);}
+        else printf(" enter  the valid position");
     return 0;
 }
