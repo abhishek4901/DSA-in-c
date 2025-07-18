70c70fdcbba1a3e77f337ef699b5ada6c81ac5d0
AႦԋιʂԋҽƙ ƙυɱαɾ ραƚҽʅ <159283155+abhishek4901@users.noreply.github.com>
2025-07-19 02:15:29 +0530

    linearSearch.c

--- Searching/linearSearch.c
+++ Searching/linearSearch.c
@@ -5,7 +5,7 @@ int linearSearch(int arr[], int n, int value) {
     for (int i = 0; i < n; i++) {
         if (arr[i] == value) {
             return i+1; 
-        }
+        } 
     }
     return -1; 
 }
