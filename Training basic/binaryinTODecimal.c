4fb050f39aa3001feffdf167b4d2c0094c25c71a
AႦԋιʂԋҽƙ ƙυɱαɾ ραƚҽʅ <159283155+abhishek4901@users.noreply.github.com>
2025-08-05 10:18:08 +0530

    .

---
+++ Training basic/binaryinTODecimal.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int b, d = 0, p = 1;
+    printf("Enter the binary number: ");
+    scanf("%d", &b);
+
+    while (b > 0) {
+        int last_digit = b % 10;
+        d = d + last_digit * p;
+        p = p * 2;
+        b = b / 10;
+    }
+
+    printf("Decimal : %d\n", d);
+    return 0;
+}
