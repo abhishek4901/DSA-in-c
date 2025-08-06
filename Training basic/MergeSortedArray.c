01614978a81a5849cb9877da6e6ca2aa5d317196
AႦԋιʂԋҽƙ ƙυɱαɾ ραƚҽʅ <159283155+abhishek4901@users.noreply.github.com>
2025-08-06 10:10:11 +0530

    .

--- Training basic/MergeSortedArray.c
+++ Training basic/MergeSortedArray.c
@@ -1,24 +1,27 @@
 #include <stdio.h>
 #define SIZE 10
 int main() {
-
-    int n=3, i, j, k;
-  
-    int arr1[n], arr2[n],Arr3[SIZE];
+    int n=3, i, j, k = -1;
+    int arr1[n], arr2[n], Arr3[SIZE];
     printf("Enter the element of first array: ");
     for(i = 0; i < n; i++) {
-         scanf("%d", &arr1[i]);
-     }
+        scanf("%d", &arr1[i]);
+    }
 
     printf("Enter the element of second array: ");
     for(i = 0; i < n; i++) {
-         scanf("%d", &arr2[i]);
-     }
+        scanf("%d", &arr2[i]);
+    }
 for(i =0, j=0; i<n && j<n; ) {
     if(arr1[i] < arr2[j]) {
         Arr3[++k] = arr1[i++];
-    } else {
+    }else if (arr1[i] > arr2[j])
+    {
         Arr3[++k] = arr2[j++];
+    }
+     else {
+        Arr3[++k] = arr1[i++];
+        j++;
     }
 }
 while(i < n) {
@@ -29,9 +32,8 @@ while(j < n) {
 }
 
     printf("Merged sorted array: ");
-for(i = 0; i < k; i++) {
-    printf("%d ", Arr3[i]) ;
-    
-}
+    for(i = 0; i <= k; i++) {
+        printf("%d ", Arr3[i]);
+    }
 return 0;
 }
