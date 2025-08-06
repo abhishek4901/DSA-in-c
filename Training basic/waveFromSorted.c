10cfad7bfe675e84d8c2858498520a562f4db53d
AႦԋιʂԋҽƙ ƙυɱαɾ ραƚҽʅ <159283155+abhishek4901@users.noreply.github.com>
2025-08-06 10:52:46 +0530

    .

---
+++ Training basic/waveFromSorted.c
@@ -0,0 +1,30 @@
+ #include<stdio.h>
+#define SIZE 10
+int main() {
+    int n; printf("Enter the size of array: ");
+    scanf("%d", &n);    
+     printf("Enter the elements of array: ");   
+    int arr[n];
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    for (int i = 0; i < n; i++)
+    {
+        if (i % 2 == 0 && i + 1 < n )
+        {
+            
+            int temp = arr[i];
+            arr[i] = arr[i + 1];
+            arr[i + 1] = temp;
+        }
+    }
+
+    
+    for (size_t i = 0; i < 5; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
