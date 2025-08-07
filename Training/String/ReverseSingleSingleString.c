b6b7ac1c1a580feccbb5ce4c6a9583654a3ddbc5
AႦԋιʂԋҽƙ ƙυɱαɾ ραƚҽʅ <159283155+abhishek4901@users.noreply.github.com>
2025-08-07 15:38:51 +0530

    .

---
+++ Training/String/ReverseSingleSingleString.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main() {
+    char s[50] = "i love my india";
+    int i, pre = 0, start, end, temp;
+
+    for (i = 0;; i++) {
+        // Check for space or null terminator to mark word boundary
+        if (s[i] == ' ' || s[i] == '\0') {
+            // The word is from pre to i-1
+            start = pre;
+            end = i - 1;
+
+            // Reverse characters in this word
+            while (start < end) {
+                temp = s[start];
+                s[start] = s[end];
+                s[end] = temp;
+                start++;
+                end--;
+            }
+
+            if (s[i] == '\0') // End of string
+                break;
+
+            pre = i + 1;  // Update start of next word
+        }
+    }
+
+    printf("Reversed words string: %s\n", s);
+    return 0;
+}
