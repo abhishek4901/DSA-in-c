a470747f8df2cb47641180c9202b10680f925f04
AႦԋιʂԋҽƙ ƙυɱαɾ ραƚҽʅ <159283155+abhishek4901@users.noreply.github.com>
2025-08-11 09:59:32 +0530

    .

--- Training/String/Anagram.c
+++ Training/String/Anagram.c
@@ -1,17 +1,21 @@
-
 #include <stdio.h>
 #include <string.h>
 
-int isAnagram(char str1[], char str2[]) {
-    int count[256] = {0};
+#define CHAR_RANGE 256
+
+int areAnagramStrings(char str1[], char str2[]) {
+    int count[CHAR_RANGE] = {0};
     int i;
+    // If lengths are not equal, they can't be anagrams
     if (strlen(str1) != strlen(str2))
         return 0;
+
     for (i = 0; str1[i] && str2[i]; i++) {
-        count[(unsigned char)str1[i]]++;
-        count[(unsigned char)str2[i]]--;
+        count[(char)tolower(str1[i])]++;
+        count[(char)tolower(str2[i])]--;
     }
-    for (i = 0; i < 256; i++) {
+
+    for (i = 0; i < CHAR_RANGE; i++) {
         if (count[i] != 0)
             return 0;
     }
@@ -20,13 +24,13 @@ int isAnagram(char str1[], char str2[]) {
 
 int main() {
     char str1[100], str2[100];
-    printf("Enter first string: ");
-    gets(str1);
-    printf("Enter second string: ");
-    gets(str2);
-    if (isAnagram(str1, str2))
-        printf("The strings are anagrams.\n");
+    printf("Enter two strings: ");
+    scanf("%s %s", str1, str2);
+
+    if (areAnagramStrings(str1, str2))
+        printf("\"%s\" and \"%s\" are anagram strings.\n", str1, str2);
     else
-        printf("The strings are not anagrams.\n");
+        printf("\"%s\" and \"%s\" are not anagram strings.\n", str1, str2);
+
     return 0;
 }
