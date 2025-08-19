a5166a6d7fd1d854932fcb0498c4172460450fdf
AႦԋιʂԋҽƙ ƙυɱαɾ ραƚҽʅ <159283155+abhishek4901@users.noreply.github.com>
2025-08-19 15:34:22 +0530

    .

--- Training/String/lengthOfString.c
+++ Training/String/lengthOfString.c
@@ -7,9 +7,14 @@ int main() {
     printf("Enter a string: ");
     fgets(str, sizeof(str), stdin);   // read input with spaces
 
-    for (i = 0; str[i] != '\0'; i++); // count length until '\0'
+    for (i = 0; str[i] != '\0'; i++); // count until '\0'
 
-    printf("Length of the string = %d\n", i); // print length
+    // ignore newline character if present
+    if (str[i-1] == '\n') {
+        i--;
+    }
+
+    printf("Length of the string = %d\n", i);
 
     return 0;
 }
